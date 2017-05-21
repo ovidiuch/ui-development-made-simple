2d24c98f32e48effeb27da8f67c80b04f5bcecaf
Ovidiu Cherecheș <skidding@gmail.com>
2017-05-21 14:55:27 +0300

    Add ability to go back by long clicking

--- components/Presentation.jsx
+++ components/Presentation.jsx
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import Slide from './Slide';
 
-const { max, min } = Math;
+const { max, min, abs } = Math;
 
 const LEFT = 37;
 const RIGHT = 39;
 
 // Use `keydown` for debugging and `keyup` when presenting!
 const KEY_EVENT = 'keydown';
+
+// How much time (ms) to keep mouse pressed to go back instead of forward
+const GO_BACK_TIMEOUT = 300;
 
 export default class Presentation extends Component {
   constructor(props) {
@@ -52,13 +55,29 @@ export default class Presentation extends Component {
     }
   };
 
-  onClick = () => {
-    this.next();
+  onMouseDown = () => {
+    this.clickTimeout = setTimeout(() => {
+      this.setState({ goingBack: true });
+    }, GO_BACK_TIMEOUT);
+  };
+
+  onMouseUp = () => {
+    clearTimeout(this.clickTimeout);
+
+    // Go back by holding and releasing
+    if (this.state.goingBack) {
+      this.prev();
+      this.setState({
+        goingBack: false,
+      });
+    } else {
+      this.next();
+    }
   };
 
   render() {
     const { slides } = this.props;
-    const { slide: slideIndex } = this.state;
+    const { slide: slideIndex, goingBack } = this.state;
     const slide = slides[slideIndex];
 
     if (!slide) {
@@ -66,7 +85,14 @@ export default class Presentation extends Component {
     }
 
     return (
-      <div className="root" onClick={this.onClick}>
+      <div
+        className="root"
+        style={{
+          opacity: goingBack ? 0.5 : 1,
+        }}
+        onMouseDown={this.onMouseDown}
+        onMouseUp={this.onMouseUp}
+      >
         <Slide {...slide} />
         <style jsx>{`
           .root {
